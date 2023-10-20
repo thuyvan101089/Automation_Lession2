0de2686c352d497e157f97a4865e2be5ea38b22c
unknown <thuyvan1010@gmail.com>
2023-10-20 23:07:54 +0700

    Lab 09

---
+++ src/lab_lession9/AnimalController.java
@@ -0,0 +1,35 @@
+package lab_lession9;
+
+import lab_lession7_2_bestpractice.Animal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnimalController {
+
+    public static List<AnimalWithBuilder> filterAnimal(List<AnimalWithBuilder> animalList) {
+
+        List<AnimalWithBuilder> filteredAnimalList = new ArrayList<>();
+
+        for (AnimalWithBuilder animals : animalList) {
+            if (!animals.isWithWings()) {
+                filteredAnimalList.add(animals);
+            } else {
+                System.out.println(animals.getName() + " with wings can not join the race");
+            }
+        }
+        return filteredAnimalList;
+    }
+
+    public static AnimalWithBuilder getWinner(List<AnimalWithBuilder> filteredAnimalList) {
+
+        AnimalWithBuilder winner = filteredAnimalList.get(0);
+        for (AnimalWithBuilder animal : filteredAnimalList) {
+            if (animal.getSpeed() > winner.getSpeed()) {
+                winner = animal;
+            }
+        }
+        return winner;
+
+    }
+}
